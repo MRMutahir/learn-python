6070e95c97273204ff33aac841a42fb13cd94e3d
MRMutahir <mutahirkareem820@gmail.com>
2025-02-07 18:18:51 -0800

    Added Python number practice programs (basic, intermediate)

--- Numbers/index.py
+++ Numbers/index.py
@@ -1,3 +1,53 @@
-a = 2
-b = 4
-c = 6
+# try:
+#    num1:int = int(input("Enter first number"))
+#    num2:int = int(input("Enter sec number"))
+#    sum = num1 + num2
+#    print(sum)
+
+# except ValueError:
+#    print("❌ Error: Please enter a valid integer number!")
+
+
+
+
+
+# try:
+#     evenOddNum = int(input("Enter a number: "))  
+    
+#     if (evenOddNum % 2) == 0:
+#         # print(evenOddNum, " is Even number")
+#         print(f"{evenOddNum} is an Even number")  # Extra space diya
+       
+#     else:
+#         print(str(evenOddNum) + "is Odd number")
+
+# except ValueError:
+#     print("❌ Invalid input! Please enter a valid integer number.")
+
+    
+
+numInt = 2 #integer 
+numFloat = 2.3 #decimal 
+
+
+
+
+import math
+
+try:
+    giveNum = float(input("Enter Number: "))  
+    print(f"Ceil value: {math.ceil(giveNum)}")  
+    print(f"Floor value: {math.floor(giveNum)}")  
+
+except ValueError:
+    print("❌ Invalid input! Please enter a valid number.")
+
+
+import math
+
+try:
+    num = int(input("Enter Number"))
+    print(f"{num} factorial {math.factorial(num)}")
+except ValueError:
+    print("❌ Invalid input! Please enter a valid number.")
+
